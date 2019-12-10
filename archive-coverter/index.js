d96b81ec15726e65ca01f1a923b8634dea824703
Cuong Le <cuongle@bitmark.com>
2019-12-10 14:55:13 +0700

    [converter] add some basic server code to upload file

--- archive-coverter/index.js
+++ archive-coverter/index.js
@@ -1,3 +1,52 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.post('/upload', async (req, res) => {
+  try {
+    if(!req.files) {
+      res.send({
+        status: false,
+        message: 'No file uploaded'
+      });
+    } else {
+      let archive = req.files.archive;
+      avatar.mv('./upload/archive.zip');
+
+      //send response
+      res.send({
+        status: true,
+        message: 'File is uploaded'
+      });
+    }
+  } catch (err) {
+      res.status(500).send(err);
+  }
+});
+
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);
+
+
+
 // API multi part
 // Unzip
 // Save post
