cf8befb396eb97b2fe8b80f47dd77dba442cb9e0
Anh Nguyen <anhnguyen@bitmark.com>
2019-12-10 15:20:39 +0700

    [converter] convert posts

--- archive-coverter/posts.js
+++ archive-coverter/posts.js
@@ -1,3 +1,15 @@
 
 
-module.exports = async function() {};
+module.exports = async function() {
+    var yourPostsData = require('./data/posts/your_posts_1.json');
+    var otherPostsData = require('./data/posts/other_people\'s_posts_to_your_timeline');
+
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(':memory:');
+
+    db.serialize(function() {
+    db.run("CREATE TABLE posts (title TEXT, type TEXT)");
+    });
+
+    db.close();
+};
